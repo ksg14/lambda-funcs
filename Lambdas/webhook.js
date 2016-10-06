ee94613fe9a875e63ad51ff89ce64529602ee57c
phiber <ksg207@live.com>
2016-10-07 00:18:37 +0530

    Webhook updated

--- Lambdas/webhook.js
+++ Lambdas/webhook.js
@@ -2,18 +2,18 @@ exports.handler = function(event, context) {
 
   try {
 
-    var verify_token = 'my_awesome_token';
+    var verify_token = 'my_awesome_token';//my token, expected to be received
 
 
     if(event.params && event.params.querystring)
     {   // for GET request
-        var queryParams = event.params.querystring;
+        var queryParams = event.params.querystring;// From Mapping template for integration request
         var VerifyToken = queryParams['hub.verify_token']
 
         if (VerifyToken === verify_token)
         {
             var challenge = queryParams['hub.challenge']
-            context.done(null, parseInt(challenge));
+            context.done(null, parseInt(challenge));//Send back challenge
         }
         else
         {
