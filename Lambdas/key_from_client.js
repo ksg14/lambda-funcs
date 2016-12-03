10e7124f95704a9405eb68668e135dcbc97d2730
Karanjit Singh <ksg207@live.com>
2016-12-03 23:39:14 +0530

    deleted files

--- Lambdas/key_from_client.js
+++
@@ -1,8 +0,0 @@
-//First ever Lamda
-console.log('Token JSON from Client');
-
-exports.handler = function(event, context){
-    //Request parameter should follow token key
-    context.succeed("Token from Client SPA - " + event.token);
-    context.done(null, 'Hello from the other side');
-};
